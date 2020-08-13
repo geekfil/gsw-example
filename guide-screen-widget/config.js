769ba4fbf364626c5e26f585648771e0bfe374b1
geekfil <geekfil@gmail.com>
2020-08-14 00:56:11 +0300

    init

---
+++ guide-screen-widget/config.js
@@ -0,0 +1,315 @@
+var guide_screen_config = {
+  "screens": [
+    {
+      "key": 1,
+      "title": "Создание служебной записки по шагам",
+      "desc": null,
+      "prev_key": null,
+      "next_key": 2,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.45.12.png",
+      "links": [
+        {
+          "title": "Подробнее..",
+          "text": "",
+          "key": 2,
+          "width": 200,
+          "height": 50,
+          "top": 509,
+          "left": 118
+        },
+        {
+          "title": "Подробнее",
+          "text": "",
+          "key": 4,
+          "width": 200,
+          "height": 50,
+          "top": 202,
+          "left": 87
+        },
+        {
+          "title": "Подробнее",
+          "text": "",
+          "key": 5,
+          "width": 200,
+          "height": 50,
+          "top": 109,
+          "left": 265
+        },
+        {
+          "title": "Подробнее",
+          "text": "",
+          "key": 6,
+          "width": 200,
+          "height": 50,
+          "top": 111,
+          "left": 515
+        },
+        {
+          "title": "Подробнее",
+          "text": "",
+          "key": 7,
+          "width": 200,
+          "height": 50,
+          "top": 46,
+          "left": 864
+        },
+        {
+          "title": "Подробнее..",
+          "text": "",
+          "key": 8,
+          "width": 200,
+          "height": 50,
+          "top": 237,
+          "left": 921
+        }
+      ]
+    },
+    {
+      "key": 2,
+      "title": "Создаем новый документ",
+      "desc": null,
+      "prev_key": 1,
+      "next_key": 3,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.45.26.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 3,
+          "width": 200,
+          "height": 50,
+          "top": 489,
+          "left": 1000
+        }
+      ]
+    },
+    {
+      "key": 3,
+      "title": "Выбираем тип служебной записки",
+      "desc": null,
+      "prev_key": 2,
+      "next_key": 1,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.46.04.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 489,
+          "left": 996
+        }
+      ]
+    },
+    {
+      "key": 4,
+      "title": "Вносим или редактируем содержание служебки",
+      "desc": null,
+      "prev_key": 1,
+      "next_key": 6,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.46.21.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 9,
+          "width": 200,
+          "height": 50,
+          "top": 477,
+          "left": 1000
+        }
+      ]
+    },
+    {
+      "key": 5,
+      "title": "Добавляем или изменяем вложения",
+      "desc": null,
+      "prev_key": 1,
+      "next_key": 6,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.12.00.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 475,
+          "left": 995
+        }
+      ]
+    },
+    {
+      "key": 6,
+      "title": "Настраиваем связи с другими документами и доступность просмотра",
+      "desc": "",
+      "prev_key": 1,
+      "next_key": 13,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.10.01.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 13,
+          "width": 200,
+          "height": 50,
+          "top": 477,
+          "left": 993
+        }
+      ]
+    },
+    {
+      "key": 7,
+      "title": "Редактируем этапы и участников согласования",
+      "desc": null,
+      "prev_key": 1,
+      "next_key": 12,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.46.38.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 12,
+          "width": 200,
+          "height": 50,
+          "top": 488,
+          "left": 1001
+        }
+      ]
+    },
+    {
+      "key": 8,
+      "title": "Доп действия (почта, файлы, удаление)",
+      "desc": null,
+      "prev_key": 2,
+      "next_key": 2,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2012.46.48.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 481,
+          "left": 990
+        }
+      ]
+    },
+    {
+      "key": 9,
+      "title": "Редактирование в Ворд",
+      "desc": null,
+      "prev_key": 4,
+      "next_key": 10,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.23.15.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 10,
+          "width": 200,
+          "height": 50,
+          "top": 490,
+          "left": 1004
+        }
+      ]
+    },
+    {
+      "key": 10,
+      "title": "Переносим текст в Босс-референт",
+      "desc": null,
+      "prev_key": 9,
+      "next_key": 11,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.23.24.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 11,
+          "width": 200,
+          "height": 50,
+          "top": 484,
+          "left": 993
+        }
+      ]
+    },
+    {
+      "key": 11,
+      "title": "Вносим название служебной записки",
+      "desc": null,
+      "prev_key": 10,
+      "next_key": 1,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.23.35.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 489,
+          "left": 1001
+        }
+      ]
+    },
+    {
+      "key": 12,
+      "title": "Этапы и согласующие",
+      "desc": null,
+      "prev_key": 1,
+      "next_key": 1,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.24.17.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 493,
+          "left": 987
+        }
+      ]
+    },
+    {
+      "key": 13,
+      "title": "Связь с документами",
+      "desc": null,
+      "prev_key": 6,
+      "next_key": 14,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.23.57.png",
+      "links": [
+        {
+          "title": "Далее",
+          "text": "",
+          "key": 14,
+          "width": 200,
+          "height": 50,
+          "top": 490,
+          "left": 997
+        }
+      ]
+    },
+    {
+      "key": 14,
+      "title": "Доступность участникам",
+      "desc": null,
+      "prev_key": 13,
+      "next_key": 1,
+      "img_src": "https://326825.selcdn.ru/guide-screen/Снимок%20экрана%202020-08-10%20в%2013.24.07.png",
+      "links": [
+        {
+          "title": "Назад",
+          "text": "",
+          "key": 1,
+          "width": 200,
+          "height": 50,
+          "top": 444,
+          "left": 994
+        }
+      ]
+    }
+  ]
+};
